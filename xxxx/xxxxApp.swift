a13600c46abeec3834022a08cfcbbead1144bc50
OlaAbdallahAli <olaali2815@gmail.com>
2022-11-02 10:38:55 +0300

    Initial Commit

---
+++ xxxx/xxxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  xxxxApp.swift
+//  xxxx
+//
+//  Created by Ola Abdallah Taha on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct xxxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xxxxUITests/xxxxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  xxxxUITestsLaunchTests.swift
+//  xxxxUITests
+//
+//  Created by Ola Abdallah Taha on 08/04/1444 AH.
+//
+
+import XCTest
+
+final class xxxxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
